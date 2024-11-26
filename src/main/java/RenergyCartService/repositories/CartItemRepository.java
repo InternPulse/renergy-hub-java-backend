6501bf1d8f9dd93b79af185975ad0b510d55fef6
Ojay95 <112874084+Ojay95@users.noreply.github.com>
2024-11-26 13:54:36 +0100

    application properties fixes

--- src/main/java/RenergyCartService/repositories/CartItemRepository.java
+++ src/main/java/RenergyCartService/repositories/CartItemRepository.java
@@ -2,7 +2,9 @@ package RenergyCartService.repositories;
 
 import RenergyCartService.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     void deleteByCartIdAndProductId(Long cartId, Long productId);
 }
